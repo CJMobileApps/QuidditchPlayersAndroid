2b62d123324886c82b9d2d25e1a34ded41fb6615
Chris Jones <chrisjmobileapps@gmail.com>
2020-07-04 19:16:30 -0400

    Add MainActivityTest

---
+++ app/src/androidTest/java/com/cjmobileapps/TestUtil.kt
@@ -0,0 +1,26 @@
+package com.cjmobileapps
+
+import android.view.View
+import androidx.annotation.NonNull
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.matcher.BoundedMatcher
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+
+fun atPosition(position: Int, @NonNull itemMatcher: Matcher<View?>): Matcher<View?>? {
+    return object : BoundedMatcher<View?, RecyclerView>(RecyclerView::class.java) {
+        override fun describeTo(description: Description) {
+            description.appendText("has item at position $position: ")
+            itemMatcher.describeTo(description)
+        }
+
+        override fun matchesSafely(view: RecyclerView): Boolean {
+            val viewHolder = view.findViewHolderForAdapterPosition(position)
+                    ?: // has no item on such position
+                    return false
+            return itemMatcher.matches(viewHolder.itemView)
+        }
+    }
+}
+
+//if recycler view helper not good enough checkout https://stackoverflow.com/questions/52737309/espresso-check-recyclerview-items-are-ordered-correctly
